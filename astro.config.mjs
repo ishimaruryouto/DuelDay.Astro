1332e04d5952590c6ae89568a2f62135acfe99da
ishimaruryouto <2240006@ecc.ac.jp>
2025-07-31 15:19:46 +0900

    ブラッシュアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 
+// import { defineConfig } from "astro/config";
+// import react from "@astrojs/react";
+
+
+// import { defineConfig } from 'astro/config';
+// export default defineConfig({
+//     output: 'server',
+// });
+
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
-
-import { defineConfig } from 'astro/config';
 export default defineConfig({
-    output: 'server',
+    output: "server",
+    adapter: vercel(),
 });
